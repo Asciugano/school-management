f86bd8c7f8a85ac472308d38958c3e2bf2dc9d1a
Asciugano <samuele.diaferio@fastwebnet.it>
2025-09-16 19:53:46 +0200

    modificata la struttura del progetto

--- src/pages/login_page.php
+++ src/pages/login_page.php
@@ -10,13 +10,16 @@ if (session_status() == PHP_SESSION_NONE) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Logout</title>
+  <title>Login</title>
   <link href="../css/style.css" rel="stylesheet">
 </head>
 
 <body>
-  <h1>Logout</h1>
-  <?php include("../nav.php") ?>
+  <h1>Login</h1>
+  <?php include("../nav.php"); ?>
+
+  <form action="../utils/login.php" method="post">
+  </form>
 </body>
 
 </html>
--- src/pages/singin_page.php
+++ src/pages/singin_page.php
@@ -10,13 +10,16 @@ if (session_status() == PHP_SESSION_NONE) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Login</title>
+  <title>Singup</title>
   <link href="../css/style.css" rel="stylesheet">
 </head>
 
 <body>
-  <h1>Login</h1>
+  <h1>Singup</h1>
   <?php include("../nav.php"); ?>
+
+  <form action="../utils/singup.php" method="post">
+  </form>
 </body>
 
 </html>
